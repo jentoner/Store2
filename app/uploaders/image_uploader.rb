12722c7a5284d34f7376039e594cb4e0ea165a11
Jennifer Toner <wilhelmena3@gmail.com>
2016-11-24 00:35:33 -0600

    added a few products

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,20 +8,20 @@ class ImageUploader < CarrierWave::Uploader::Base
   #  storage :fog
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
-  version :resized do
-    # returns an image with a maximum width of 100px
-    # while maintaining the aspect ratio
-    # 10000 is used to tell CW that the height is free
-    # and so that it will hit the 100 px width first
-    process resize_to_fit: [300, 100_00]
-  end
+  # version :resized do
+  # returns an image with a maximum width of 100px
+  # while maintaining the aspect ratio
+  # 10000 is used to tell CW that the height is free
+  # and so that it will hit the 100 px width first
+  # process resize_to_fit: [300, 100_00]
+  # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_limit: [200, 200]
-  end
+  # version :thumb do
+  # process resize_to_limit: [200, 200]
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
